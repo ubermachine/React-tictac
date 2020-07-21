472709cbf58082ca796d61275eb1cc75c7c42356
ubermachine <shivamkatoch@outlook.com>
2020-07-22 02:01:59 +0530

    added basic functionality'

--- src/index.js
+++ src/index.js
@@ -2,19 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; 
 class Square extends React.Component {
+
+        
+    
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={()=>this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return (<Square value={this.state.squares[i]}
+    onClick={()=>this.handleClick(i)}/>)
+  };
 
   render() {
     const status = 'Next player: X';
