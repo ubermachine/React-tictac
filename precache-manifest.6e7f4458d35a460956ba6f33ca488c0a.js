06cf2e5ae424d299f86f158e970aa2b832b96fe6
ubermachine <shivamkatoch@outlook.com>
2020-07-22 21:58:51 +0530

    Updates

---
+++ precache-manifest.6e7f4458d35a460956ba6f33ca488c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473ed99536b3bf2193451b338106c8e7",
+    "url": "/React-tictac/index.html"
+  },
+  {
+    "revision": "f3835cf1bc9427d3b828",
+    "url": "/React-tictac/static/css/main.1786e78b.chunk.css"
+  },
+  {
+    "revision": "185139b823981a9ca5c6",
+    "url": "/React-tictac/static/js/2.4eeac0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-tictac/static/js/2.4eeac0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3835cf1bc9427d3b828",
+    "url": "/React-tictac/static/js/main.12cec469.chunk.js"
+  },
+  {
+    "revision": "0faf0ec37201af721ae5",
+    "url": "/React-tictac/static/js/runtime-main.ea41931b.js"
+  }
+]);
